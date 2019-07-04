3aca0e5e75c0621fddcb1e1f545d405961a5d4f6
kz1324 <kz1324@naver.com>
2019-07-04 11:48:21 +0900

    0704

--- backend/web/src/main/java/com/tardy/web/controller/BoardController.java
+++ backend/web/src/main/java/com/tardy/web/controller/BoardController.java
@@ -8,6 +8,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
 import com.tardy.web.domain.BoardDTO;
 import com.tardy.web.entities.Board;
 import com.tardy.web.repositories.BoardRepository;
@@ -17,7 +19,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,12 +37,10 @@ public class BoardController {
     @Autowired BoardRepository repo;
     @Autowired ModelMapper modelMapper;
     
-
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
-
 
     //1. 게시판글쓰기 먼저 
     @PostMapping("")
@@ -71,7 +73,7 @@ public class BoardController {
 
     
     //2. 게시판 리스트 출력하기
-    @GetMapping("list")
+    @GetMapping("/list")
     public List<BoardDTO> selectList(){
         System.out.println("리스트 출력 메소드");
         Iterable<Board> board = repo.findAll();
@@ -85,5 +87,71 @@ public class BoardController {
         return list;
     }
 
+
+    //3. 게시판 수정
+    @PutMapping("/{id}")
+    public HashMap<String, String> updateList(@PathVariable String id, @RequestBody BoardDTO dto) {
+        System.out.println("update컨트롤러로 옴");
+        System.out.println("id : " + id);
+        System.out.println("dto : " + dto);
+        
+        Board entity = repo.findById(Long.parseLong(id)).get();
+        System.out.println("레포에서온 엔티티 : "+entity);
+        entity.setTitle(dto.getTitle());
+        entity.setContent(dto.getContent());
+       
+        repo.save(entity);
+
+        HashMap map = new HashMap<>();
+        map.put("result", "SUCCESS");
+        
+        return map;
+    }
+
+    // public void updateList(@PathVariable String id, @RequestBody BoardDTO dto) {
+    //     System.out.println("update컨트롤러로 옴");
+    //     System.out.println("id : " + id);
+    //     System.out.println("dto : " + dto);
+        
+    //     // Board entity = repo.findById(Long.parseLong(id)).get();
+    //     // entity.setTitle(dto.getTitle());
+    //     // entity.setContent(dto.getContent());
+       
+    //     // repo.save(entity);
+
+    //     // HashMap map = new HashMap<>();
+    //     // map.put("result", "SUCCESS");
+    //     // return map;
+    // }
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    //3. 게시판 수정2
+    @GetMapping("/update")
+    public HashMap<String, String> updateList2(@RequestBody BoardDTO dto) {
+        System.out.println("update컨트롤러로 옴");
+        System.out.println("dto : " + dto);
+        
+        Board entity = repo.findById(Long.parseLong(dto.getId())).get();
+        entity.setTitle(dto.getTitle());
+        entity.setContent(dto.getContent());
+       
+        repo.save(entity);
+
+        HashMap map = new HashMap<>();
+        map.put("result", "SUCCESS");
+        return map;
+    }
     
 }
